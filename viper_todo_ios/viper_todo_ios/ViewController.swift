ea00b38a7da2392e1fdf8400d9fe2774a818ce9f
younippon <hikonyan.mh36@gmail.com>
2017-10-08 18:23:53 +0900

    first commit

---
+++ viper_todo_ios/viper_todo_ios/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  viper_todo_ios
+//
+//  Created by 佐々木耀 on 2017/10/08.
+//  Copyright © 2017年 Yo Sasaki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
